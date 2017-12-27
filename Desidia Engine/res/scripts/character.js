c3d345c7a901c721c046b481300d86c4111021a6
Iwo Plaza <gobbobpl@gmail.com>
2017-12-27 18:39:22 +0100

    Added a walking bob animation.

--- Desidia Engine/res/scripts/character.js
+++ Desidia Engine/res/scripts/character.js
@@ -1,9 +1,12 @@
 Engine.registerEventListener('init', function (e) {
+    e.metadata.walking = false;
+    e.metadata.walkingBob = 0;
     e.metadata.cameraTransform = GameObject.getTransform("mainCamera");
     if (!e.metadata.cameraTransform) {
         Engine.print("The \"mainCamera\" couldn't be found in the scene.");
         return e;
     }
+    e.metadata.originalCameraLocation = Transform.getLocation(e.metadata.cameraTransform);
 
     var transform = GameObject.getTransform(e.gameObject);
     e.metadata.yaw = 0.9;
@@ -19,14 +22,31 @@ function processInput(e, transform, rigidbody) {
     var forwardVector = Transform.getForwardVector(transform);
     var rightVector = Transform.getRightVector(transform);
 
-    if (Input.isKeyDownCase('A'.charCodeAt(0))) // A
+    e.metadata.walking = false;
+
+    if (Input.isKeyDownCase('A'.charCodeAt(0))) { // A
         ComponentRigidbody.addForce(rigidbody, rightVector.multiply(moveSpeed));
-    if (Input.isKeyDownCase('D'.charCodeAt(0))) // D
+        e.metadata.walking = true;
+    }
+    if (Input.isKeyDownCase('D'.charCodeAt(0))) { // D
         ComponentRigidbody.addForce(rigidbody, rightVector.multiply(-moveSpeed));
-    if (Input.isKeyDownCase('W'.charCodeAt(0))) // W
+        e.metadata.walking = true;
+    }
+    if (Input.isKeyDownCase('W'.charCodeAt(0))) { // W
         ComponentRigidbody.addForce(rigidbody, forwardVector.multiply(moveSpeed));
-    if (Input.isKeyDownCase('S'.charCodeAt(0))) // S
+        e.metadata.walking = true;
+    }
+    if (Input.isKeyDownCase('S'.charCodeAt(0))) { // S
         ComponentRigidbody.addForce(rigidbody, forwardVector.multiply(-moveSpeed));
+        e.metadata.walking = true;
+    }
+
+    var velocity = ComponentRigidbody.getVelocity(rigidbody);
+    velocity.y = 0;
+    if (e.metadata.walking) {
+        e.metadata.walkingBob += Engine.Time.delta * velocity.length() * 0.02;
+        e.metadata.walkingBob %= 1;
+    }
 
     if (Input.isKeyDown(32)) // Space
         ComponentRigidbody.addForce(rigidbody, new Vector3(0, 2, 0));
@@ -47,11 +67,6 @@ Engine.registerEventListener('mouseMove', function (e) {
             e.metadata.pitch = 1.5;
         if (e.metadata.pitch < -1.5)
             e.metadata.pitch = -1.5;
-
-        Transform.resetOrientation(transform);
-        Transform.resetOrientation(e.metadata.cameraTransform);
-        Transform.rotateY(transform, -e.metadata.yaw);
-        Transform.rotateX(e.metadata.cameraTransform, -e.metadata.pitch);
     }
     return e;
 });
@@ -66,6 +81,18 @@ Engine.registerEventListener('update', function (e) {
     velocity.x *= 0.9;
     velocity.z *= 0.9;
     ComponentRigidbody.setVelocity(rigidbody, velocity);
+
+    Transform.resetOrientation(transform);
+    Transform.resetOrientation(e.metadata.cameraTransform);
+    Transform.rotateY(transform, -e.metadata.yaw);
+    Transform.rotateX(e.metadata.cameraTransform, -e.metadata.pitch);
+    Transform.rotateZ(e.metadata.cameraTransform, Math.cos(e.metadata.walkingBob * Math.PI * 2) * 0.02);
+
+    if (e.metadata.walking) {
+        var cameraLocation = Object.create(e.metadata.originalCameraLocation);
+        cameraLocation.y += Math.abs(Math.sin(e.metadata.walkingBob * Math.PI*2)) * 0.1;
+        Transform.setLocation(e.metadata.cameraTransform, cameraLocation);
+    }
 
     return e;
 });
